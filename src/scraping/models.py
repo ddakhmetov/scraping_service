cf8362ffeb1905224381040b0316a16bca5c5121
den <ddakhmetov@gmail.com>
2021-01-04 23:46:58 +0600

    model Language

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,5 +2,28 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50) #Charfield в отличие от TextField такое текстовое поле, которое ограничивается количеством символов
-    slug = models.CharField(max_length=50, blank=True) #blank=True означает, что поле может быть пустое
+    name = models.CharField(max_length=50,
+                            verbose_name="Название населённого пункта",
+                            unique=True)  #Charfield в отличие от TextField такое текстовое поле, которое ограничивается количеством символов
+    slug = models.CharField(max_length=50, blank=True, unique=True)  #blank=True означает, что поле может быть пустое
+    
+    class Meta:
+        verbose_name = 'Название населённого пункта'    #Отображение имени таблицы в едиственном числе
+        verbose_name_plural = 'Название населённых пунктов'     #Отображение имени таблицы во множественном числе
+
+    def __str__(self):
+        return self.name    # Отображение объекта в виде имени города
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Язык программирования",
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
