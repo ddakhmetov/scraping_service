2c5baff7343755465d76a54ec5c7c4a0ad163f96
Akhmetov Denis <ddakhmetov@gmail.com>
2021-02-14 02:05:52 +0600

    added register_done, account_settings, delete_account, messages

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from accounts.forms import UserLoginForm, UserRegistrationForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib import messages
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
+
+User = get_user_model()     # Тут получаем пользователя, чтоб можно было его потом удалить
 
 def login_vew(request):
     form = UserLoginForm(request.POST or None)
@@ -26,7 +29,38 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Профиль успешно создан')
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
 
 
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                messages.success(request, 'Изменения успешно сохранены')
+                return redirect('accounts:update')
+        # else:
+        form = UserUpdateForm(initial={'city': user.city,
+                                       'language': user.language,
+                                       'send_email': user.send_email})
+        return render(request, 'accounts/update.html', {'form': form})
+    else:
+        return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)   # Получаем именно того пользователя, который зарегистрировался, и удаляем
+            qs.delete()
+            messages.warning(request, 'Профиль удалён безвозвратно :-(')
+    return redirect('home')
